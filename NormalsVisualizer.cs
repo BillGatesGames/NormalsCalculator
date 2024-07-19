d0033c46cc1d0ca2a056efbc95525e92eadac1b7
BillGatesGames <artpost-x@mail.ru>
2024-07-19 13:16:37 +0500

    improvements

--- NormalsVisualizer.cs
+++ NormalsVisualizer.cs
@@ -1,16 +1,24 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(MeshFilter))]
 public class NormalsVisualizer : Editor
 {
-    private const string EDITOR_PREF_KEY = "normals_length";
+    [SerializeField] private float _normalsLength = 1f;
+    [SerializeField] private bool _showIndices;
+    [SerializeField] private bool _showNormalsOnlyForSplitVertices;
+
     private Mesh _mesh;
     private MeshFilter _meshFilter;
     private Vector3[] _verts;
     private Vector3[] _normals;
-    private float _normalsLength = 1f;
-    private bool _showIndices;
+    private Dictionary<Vector3, int> _splittedVertices;
+
+    private void OnDisable()
+    {
+        EditorPrefs.SetString(typeof(NormalsVisualizer).ToString(), EditorJsonUtility.ToJson(this));
+    }
 
     private void OnEnable()
     {
@@ -21,7 +29,7 @@ public class NormalsVisualizer : Editor
             _mesh = _meshFilter.sharedMesh;
         }
 
-        _normalsLength = EditorPrefs.GetFloat(EDITOR_PREF_KEY);
+        EditorJsonUtility.FromJsonOverwrite(EditorPrefs.GetString(typeof(NormalsVisualizer).ToString()), this);
     }
 
     private void OnSceneGUI()
@@ -36,15 +44,43 @@ public class NormalsVisualizer : Editor
         
         _verts = _mesh.vertices;
         _normals = _mesh.normals;
-        
+
+        _splittedVertices = new Dictionary<Vector3, int>();
+
+        if (_showNormalsOnlyForSplitVertices)
+        {
+            for (int i = 0; i < _mesh.vertexCount; i++)
+            {
+                if (!_splittedVertices.ContainsKey(_verts[i]))
+                {
+                    _splittedVertices.Add(_verts[i], 0);
+                }
+
+                _splittedVertices[_verts[i]]++;
+            }
+        }
+
         for (int i = 0; i < _mesh.vertexCount; i++)
         {
-            var pos = _verts[i] + _normals[i] * _normalsLength;
-            Handles.DrawLine(_verts[i], pos);
+            bool show = true;
 
-            if (_showIndices)
+            if (_showNormalsOnlyForSplitVertices)
             {
-                Handles.Label(pos, i.ToString());
+                if (_splittedVertices[_verts[i]] < 2)
+                {
+                    show = false;
+                }
+            }
+
+            if (show)
+            {
+                var pos = _verts[i] + _normals[i] * _normalsLength;
+                Handles.DrawLine(_verts[i], pos);
+
+                if (_showIndices)
+                {
+                    Handles.Label(pos, i.ToString());
+                }
             }
         }
     }
@@ -52,12 +88,16 @@ public class NormalsVisualizer : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+
         EditorGUI.BeginChangeCheck();
+
         _normalsLength = EditorGUILayout.FloatField("Normals Length", _normalsLength);
         _showIndices = EditorGUILayout.Toggle("Show Indices", _showIndices);
+        _showNormalsOnlyForSplitVertices = EditorGUILayout.Toggle("Show Normals Only For Split Vertices", _showNormalsOnlyForSplitVertices);
+
         if (EditorGUI.EndChangeCheck())
         {
-            EditorPrefs.SetFloat(EDITOR_PREF_KEY, _normalsLength);
+            EditorPrefs.SetString(typeof(NormalsVisualizer).ToString(), EditorJsonUtility.ToJson(this));
         }
     }
 }
